0f3f5182cacbddbb476c5cb4974a37de92e5c3ea
Dillon Fagan <dillonfagan@pm.me>
2020-12-26 23:59:45 -0500

    Add Italic and bind title

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,6 +2,7 @@
     import save from "$library/save";
     import Command from "$components/Command.svelte";
     import Bold from "$components/icons/Bold.svelte";
+    import Italic from "$components/icons/Italic.svelte";
     import AlignLeft from "$components/icons/AlignLeft.svelte";
     import Save from "$components/icons/Save.svelte";
 
@@ -15,7 +16,7 @@
 
 <div class="w-screen flex flex-row space-x-4 justify-center p-10">
     <div class="w-1/2 min-h-screen p-8 rounded bg-black shadow flex flex-col space-y-4">
-        <h1 contenteditable="true" class="font-bold text-white text-4xl" >{doc.title}</h1>
+        <h1 bind:textContent={doc.title} contenteditable="true" class="font-bold text-white text-4xl" />
         <textarea
             bind:value={doc.prose}
             placeholder="Start typing..."
@@ -23,6 +24,7 @@
     </div>
     <div class="space-y-4">
         <Command on:click={embolden}><Bold class="h-6 w-6" /></Command>
+        <Command><Italic class="h-6 w-6" /></Command>
         <Command><AlignLeft class="h-6 w-6" /></Command>
         <Command on:click={() => save(doc)}><Save class="h-6 w-6" /></Command>
     </div>
