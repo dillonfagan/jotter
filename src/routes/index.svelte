349ecae0d12a65a791be008c8ee14086be7b8da9
Dillon Fagan <dillonfagan@pm.me>
2020-12-26 23:56:24 -0500

    Add Bold button

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,7 @@
 <script>
     import save from "$library/save";
     import Command from "$components/Command.svelte";
+    import Bold from "$components/icons/Bold.svelte";
     import AlignLeft from "$components/icons/AlignLeft.svelte";
     import Save from "$components/icons/Save.svelte";
 
@@ -8,6 +9,8 @@
         title: "New document",
         prose: ""
     };
+
+    const embolden = () => document.execCommand('bold');
 </script>
 
 <div class="w-screen flex flex-row space-x-4 justify-center p-10">
@@ -19,6 +22,7 @@
             class="w-full h-full bg-transparent text-gray-50 font-medium text-xl leading-loose resize-none" />
     </div>
     <div class="space-y-4">
+        <Command on:click={embolden}><Bold class="h-6 w-6" /></Command>
         <Command><AlignLeft class="h-6 w-6" /></Command>
         <Command on:click={() => save(doc)}><Save class="h-6 w-6" /></Command>
     </div>
