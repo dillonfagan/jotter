c619ae4e5258458989f1b71b9fe03cf0f856d9ea
Dillon Fagan <dillonfagan@pm.me>
2020-12-27 11:08:44 -0500

    Introduce markdown preview

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,6 @@
 <script>
     import { onMount } from "svelte";
+    import showdown from "showdown";
     import settings from "$data/settings";
     import { loadSettings } from "$data/settings";
     import save from "$library/save";
@@ -11,23 +12,43 @@
     import AlignLeft from "$components/icons/AlignLeft.svelte";
     import Save from "$components/icons/Save.svelte";
 
+    const converter = new showdown.Converter({
+        smoothLivePreview: true,
+        noHeaderId: true,
+        requireSpaceBeforeHeadingText: true,
+        simpleLineBreaks: true,
+        headerLevelStart: 2
+    });
+
     let doc = {
         title: "New document",
         prose: ""
     };
 
+    let showPreview = false;
+    let formatted = "";
+
     onMount(() => {
         loadSettings();
     });
+
+    function format() {
+        formatted = converter.makeHtml(doc.prose);
+    }
 </script>
 
 <div class="w-screen flex flex-row space-x-4 justify-center p-10">
     <div class="page min-h-screen p-8 rounded bg-gray-50 dark:bg-black shadow flex flex-col space-y-4">
         <h1 bind:textContent={doc.title} contenteditable="true" class="font-bold text-black dark:text-white text-4xl" />
-        <textarea
-            bind:value={doc.prose}
-            placeholder="Start typing..."
-            class="w-full h-full bg-transparent text-gray-800 dark:text-gray-50 font-medium text-xl leading-loose resize-none" />
+        {#if showPreview}
+            {@html formatted}
+        {:else}
+            <textarea
+                bind:value={doc.prose}
+                on:input={format}
+                placeholder="Start typing..."
+                class="w-full h-full bg-transparent outline-none text-gray-800 dark:text-gray-50 font-medium text-xl leading-loose resize-none" />
+        {/if}
     </div>
     <div class="space-y-4">
         <Command on:click={() => settings.dark = !settings.dark}>
@@ -40,6 +61,7 @@
         <Command><Bold class="h-6 w-6" /></Command>
         <Command><Italic class="h-6 w-6" /></Command>
         <Command><AlignLeft class="h-6 w-6" /></Command>
+        <Command on:click={() => showPreview = !showPreview}>P</Command>
         <Command on:click={() => save(doc)}><Save class="h-6 w-6" /></Command>
     </div>
 </div>
